8f35b24475504913f4ed8b63a577889a236270ba
Adriel <ddeellaa@live.se>
2021-12-14 17:23:39 +0100

    Added tools for box counting (WIP)

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from numpy import sin, cos, arctan, pi, absolute
 
 
 class Fractal:
@@ -34,12 +35,61 @@ class Fractal:
         plt.savefig("../figures/{0}.png".format(self.name))
         plt.show()
 
+    def scale(self, k):
+        if self.data[k, 0] == 0.:
+            return self.data[k, 2]
+        elif self.data[k, 2] == 0.:
+            return self.data[k, 0]
+        else:
+            x = arctan(self.data[k, 2]/self.data[k, 0])
+            while True:
+                r = self.data[k, 2]/sin(x)
+                s = self.data[k, 0]/cos(x)
+                print(absolute(s - r))
+                if absolute(s - r) <= 0.0001:
+                    return s
+                else:
+                    x += pi/2
+
+    def fractal_dim(self):
+        return
+
+    def box_count(self, res):
+        x_min = np.amin(self.x)
+        x_max = np.amax(self.x)
+        y_min = np.amin(self.y)
+        y_max = np.amax(self.y)
+        step = min(abs(x_max - x_min)/res, abs(y_max - y_min)/res)
+        boxes = 0
+        box_total = 0
+        x_pos = x_min
+        y_pos = y_min
+        while x_pos < x_max:
+            i = 0
+            x_indices = []
+            for x_coordinate in self.x:
+                if np.logical_and(x_coordinate >= x_pos, x_coordinate < x_pos + step):
+                    x_indices.append(i)
+                i += 1
+            while y_pos < y_max:
+                for j in x_indices:
+                    if np.logical_and(self.y[j] >= y_pos, self.y[j] < y_pos + step):
+                        boxes += 1
+                        break
+                box_total += 1
+                y_pos += step
+            x_pos += step
+            y_pos = y_min
+        return boxes, box_total
+
 
 def main():
     test = Fractal("../data/tree")
     print(test.data)
     print(test.data.shape)
+    print(test.scale(2))
     test.ran_it()
+    print(test.box_count(10))
     test.plotting()
 
 
