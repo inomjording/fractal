8681c75526be3dbbbd71800d9f6f2dbba23c9202
Adriel <ddeellaa@live.se>
2021-12-29 14:01:16 +0100

    Updated figure style

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-from numpy import sin, cos, arctan, pi, absolute
 
 
 class Fractal:
-    def __init__(self, file_name, numits=10000):
+    def __init__(self, file_name, numits=100000):
         self.name = os.path.basename(file_name)
         self.data = np.loadtxt(file_name)
         self.x = np.empty(numits)
@@ -31,88 +30,15 @@ class Fractal:
         fig, ax = plt.subplots()
         ax.set_title(self.name.capitalize())
         ax.set_aspect('equal', 'box')
-        plt.scatter(self.x[:self.n], self.y[:self.n], marker=".", c='k')
+        plt.scatter(self.x[:self.n], self.y[:self.n], marker=".", c='k', linewidths=0.1, edgecolors='c', s=1)
         plt.savefig("../figures/{0}.png".format(self.name), bbox_inches="tight")
         plt.show()
 
-    def scale(self, k):
-        if self.data[k, 0] == 0.:
-            return self.data[k, 2]
-        elif self.data[k, 2] == 0.:
-            return self.data[k, 0]
-        else:
-            x = arctan(self.data[k, 2]/self.data[k, 0])
-            while True:
-                r = self.data[k, 2]/sin(x)
-                s = self.data[k, 0]/cos(x)
-                print(absolute(s - r))
-                if absolute(s - r) <= 0.0001:
-                    return s
-                else:
-                    x += pi/2
-
-    def fractal_dim(self):
-        return
-
-    def box_count(self, res):
-        x_min = np.amin(self.x)
-        x_max = np.amax(self.x)
-        y_min = np.amin(self.y)
-        y_max = np.amax(self.y)
-        step = min(abs(x_max - x_min)/res, abs(y_max - y_min)/res)
-        boxes = 0
-        box_total = 0
-        x_pos = x_min
-        y_pos = y_min
-        while x_pos < x_max:
-            i = 0
-            x_indices = []
-            for x_coordinate in self.x:
-                if np.logical_and(x_coordinate >= x_pos, x_coordinate < x_pos + step):
-                    x_indices.append(i)
-                i += 1
-            while y_pos < y_max:
-                for j in x_indices:
-                    if np.logical_and(self.y[j] >= y_pos, self.y[j] < y_pos + step):
-                        boxes += 1
-                        break
-                box_total += 1
-                y_pos += step
-            x_pos += step
-            y_pos = y_min
-        return boxes, step
-
-    def box_count_dim(self, res):
-        boxes, step = self.box_count(res)
-        return np.log(1/step)/np.log(boxes)
-
-    def box_counter(self):
-        boxes = []
-        magnification = []
-        i = 10
-        while i <= 100:
-            box, step = self.box_count(i)
-            boxes.append(np.log(box))
-            magnification.append(np.log(1/step))
-            i += 10
-        return np.array(boxes), np.array(magnification)
-
-    def box_count_plot(self):
-        boxes, magnification = self.box_counter()
-        print(boxes, '\n', magnification)
-        magnification = magnification[:, np.newaxis]
-        p, _, _, _ = np.linalg.lstsq(magnification, boxes)
-        plt.plot(magnification, boxes, 'o')
-        plt.plot(magnification, magnification*p)
-        plt.show()
-        print(p)
-
 
 def main():
-    test = Fractal("../data/dragon segment")
+    test = Fractal("../data/branches")
     test.ran_it()
     test.plotting()
-    # test.box_count_plot()
 
 
 if __name__ == "__main__":
